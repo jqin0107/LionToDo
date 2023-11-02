d505d6f76aab32876ad784eb52126e93c3988bf3
Yanxi Chen <ireneisabug@Yanxis-MacBook-Pro.local>
2023-11-01 20:10:35 -0400

    debug3.1

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -43,19 +43,19 @@ class EventsController < ApplicationController
       end
     
       def create
-        @event = Event.create!(event_params)
+        # @event = Event.create!(event_params)
         #@event.update(creator_id: session[:user_id])
-        flash[:notice] = "#{@event.title} was successfully created."
-        # begin
-        #   @event = Event.create!(event_params)
-        #   @event.update(creator_id: session[:user_id])
-        # rescue => exception
-        #   flash[:notice] = "Some fields are empty, event cannot be created."
-        # else
-        #   flash[:notice] = "#{@event.title} was successfully created."
+        # flash[:notice] = "#{@event.title} was successfully created."
+        begin
+          @event = Event.create!(event_params)
+          @event.update(creator_id: session[:user_id])
+        rescue => exception
+          flash[:notice] = "Cannot contain empty field."
+        else
+          flash[:notice] = "#{@event.title} was successfully created."
 
-        # end
-        # ActivityUserRelation.create!(session[:user_id], @event.id)
+        end
+        #ActivityUserRelation.create!(session[:user_id], @event.id)
         
         redirect_to events_path
       end
